63ae5ba50853b8c85d336c60506f5840ffdffe0f
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-04 15:36:47 +0300

    Enhanced Version

---
+++ index.js
@@ -0,0 +1,9 @@
+let mega = document.getElementById("link");
+
+function changeStyle() {
+    if(document.getElementById("show").style.opacity === '1') {
+        document.getElementById("show").style.opacity  = '0';
+    } else {
+        document.getElementById("show").style.opacity  = '1'
+    }
+  }
