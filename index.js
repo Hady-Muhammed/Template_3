994b1f74f414d664574a34d7b31fb48cc739fd29
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-21 00:13:23 +0300

    Enhanced

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function changeStyle() {
         document.getElementById("show").style.opacity  = '1'
     }
   }
+
+let timer1 = document.getElementsByClassName("seconds")[0]
+let timer2 = document.getElementsByClassName("minutes")[0]
+setInterval(function(){
+    if(timer1.innerHTML == 0) {
+        timer2.innerHTML -= 1;
+        timer1.innerHTML = 60;
+    }
+    else {
+        timer1.innerHTML -= 1;
+    } 
+},1000)
